a593d4c0e43fbdef04e3c314030570d7fc44c955
Yousef Hamza <jo.adam.93@gmail.com>
2017-05-22 13:43:26 +0200

    Initial Commit

---
+++ JojoDataSources/JojoDataSources.h
@@ -0,0 +1,19 @@
+//
+//  JojoDataSources.h
+//  JojoDataSources
+//
+//  Created by Yousef Hamza on 5/22/17.
+//  Copyright © 2017 yousefhamza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JojoDataSources.
+FOUNDATION_EXPORT double JojoDataSourcesVersionNumber;
+
+//! Project version string for JojoDataSources.
+FOUNDATION_EXPORT const unsigned char JojoDataSourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JojoDataSources/PublicHeader.h>
+
+
